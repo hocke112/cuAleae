fa9399bb4d37ad9a54190cea05ab388329425fa2
hocke112 <hocke112@umn.edu>
2025-12-21 19:26:32 -0600

    Added error codes to encode device-side errors

--- inc/simulation.h
+++ inc/simulation.h
@@ -9,6 +9,11 @@
 #define PRINT_TRACE      4
 #define PRINT_STATES     8
 
+// Error codes
+#define REAC_BOUND_ARR_ERR 1
+#define PROD_BOUND_ARR_ERR 2
+#define THRESH_CODE_ERR    3
+
 // extern "C" void simulation_master(unsigned int *post_trial_chem_amounts,
 //                                     chem_arr_t chem_arrays_h, field_t reactants_h, field_t products_h, float *rates_h,
 //                                     unsigned int num_chems, unsigned int num_reactions,
@@ -17,7 +22,8 @@
 //                                     unsigned int *triggered_thresh, bool *within_threshold,
 //                                     unsigned int trial_num, int *err);
 
-extern "C" void simulation_master(unsigned int *post_trial_chem_amounts, crn_t crn_h, sim_params_t sim_params, output_stats_t *out_stats,
+extern "C" void simulation_master(unsigned int *post_trial_chem_amounts, unsigned int *total_triggered_threshs_h, crn_t crn_h,
+                                        sim_params_t sim_params, output_stats_t *out_stats,
                                         unsigned int *triggered_thresh, bool *within_threshold, unsigned int trial_num, int *err);
 
 #endif
